0ec7b3e06bcb952e50eeb6290da3187c4ff3dcaa
tamitakada <tamitakada@gmail.com>
2021-10-17 19:24:14 -0400

    added randomized main

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 struct cat {
     char name[100];
@@ -12,16 +13,35 @@ void print_list(struct cat *c);
 struct cat * insert_front(struct cat *next, char *name, int age);
 struct cat * free_list(struct cat *c);
 
-int main() {
-    struct cat test;
-    test.age = 10;
-    strcpy(test.name, "Cat 1");
-    test.next = 0;
+void populate_random_arr(char *arr, int len);
 
+int main() {
+    srand(time(NULL));
+        
+    int rand_age = rand() % 100;
+        
+    int len = (rand() % 95) + 1;
+    char rand_name[len];
+    populate_random_arr(rand_name, len);
+    
+    struct cat test;
+    test.age = rand_age;
+    strcpy(test.name, rand_name);
+    test.next = 0;
+    
     struct cat *cat_chain = &test;
     
-    cat_chain = insert_front(cat_chain, "Cat 0", 2);
-    cat_chain = insert_front(cat_chain, "Cat -1", 5);
+    int i;
+    int len_chain = (rand() % 20) + 1;
+    for (i = 0; i < len_chain; i++) {
+        int name_len = (rand() % 95) + 1;
+        char name[name_len];
+        populate_random_arr(name, name_len);
+        
+        int age = rand() % 100;
+        
+        cat_chain = insert_front(cat_chain, name, age);
+    }
 
     print_list(cat_chain);
 
@@ -46,9 +66,22 @@ struct cat * insert_front(struct cat *next, char *name, int age) {
 }
 
 struct cat * free_list(struct cat *c) {
+    struct cat *next = (c->next);
+    free(c);
+    c = next;
     while (c->next) {
+        next = c->next;
         free(c);
-        c = (c->next);
+        c = next;
     }
     return c;
+}
+
+void populate_random_arr(char *arr, int len) {
+    int i;
+    for (i = 0; i < len; i++) {
+        arr[i] = (rand() % 24) + 65;
+    }
+    
+    arr[len - 1] = '\0';
 }
